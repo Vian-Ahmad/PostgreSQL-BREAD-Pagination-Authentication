332474e0713fa068ad1910f93541b3dff24be329
vian <novian701@gmail.com>
2023-12-21 23:19:05 +0700

    add phase 1 done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require ('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/', (req, res) => {
+  res.render('index', {title: 'Express'})
+})
+
+router.get('/add', (req, res) => {
+  res.render('add')
+})
 
 module.exports = router;
